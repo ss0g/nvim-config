1526bce0134cd81c427a27340669e0c71c9e31e5
Troy Edwards <tntjedi.2006@gmail.com>
2024-10-06 10:44:09 -0700

    transparency

--- lua/troye/lazy.lua
+++ lua/troye/lazy.lua
@@ -26,7 +26,13 @@ require("lazy").setup({
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {},
+		opts = {
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent"
+            }
+        },
 	},
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{
